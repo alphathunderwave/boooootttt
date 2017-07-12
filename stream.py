40ad5aebcaab696cbe89b1a40a086e9e40e1a188
alphathunderwave <bdlipp@buffalo.edu>
2017-07-12 14:49:28 -0400

    fixed weird multipule replies

--- stream.py
+++ stream.py
@@ -13,7 +13,6 @@ import boooootttt as bot
 
 bot_ = bot.bot()
 count = 0
-dev = ['beeeeennnn_']
 
 """StdOutListener is a class that listens for replies to the bot. the on status
 	function runs any time the listener 'hears' a status aimed at the bot"""
@@ -34,46 +33,12 @@ class StdOutListener(StreamListener):
 
 		if status.user.screen_name == 'boooootttt_':
 			pass
-		for user in bot_.blacklist:
-			if status.user.screen_name == user:
-				pass
+
 		else:
-			if status.user.screen_name in dev:
+			"""this is the general reply for anyone who tagged the bot"""
 
-				"""these are developer options.
-					add_dev: adds a dev to the list of developers.
-					remove_dev: removes a dev from the list.
-					dump: runs tweetdumper.
-					tweet: will make the bot tweet.
-					at: will make the bot tweet at an intended user"""
-
-				if 'add_dev' in status.text:
-					text = status.text.split(' ')
-					if text[2] not in dev:
-						dev.append(text[2])
-				elif 'remove_dev' in status.text:
-					if status.text[2] in dev:
-						dev.pop(status.text[2])
-
-				elif 'dump' in status.text:
-					dump = bot_.dump()
-					bot_.reply(status,str(dump))
-
-				elif 'tweet' in status.text:
-					response = markov.generate_markov_text()
-					bot_.do_tweet(response)
-
-				elif 'at' in status.text:
-					text = status.text.split(' ')
-					response = '@' + text[2] + ' ' + markov.generate_markov_text()
-					bot_.do_tweet(response)
-
-			else:
-
-				"""this is the general reply for anyone who tagged the bot"""
-
-				response = markov.generate_markov_text()
-				bot_.reply(status,str(response))
+			response = markov.generate_markov_text()
+			bot_.reply(status,str(response))
 
 		return True
 
