28e4eb6702e7f64d7e33ac387147058a22d75f05
alphathunderwave <bdlipp@buffalo.edu>
2017-07-15 23:39:22 -0400

    changed scheduler back to a while loop

--- run.py
+++ run.py
@@ -1,33 +1,26 @@
 """import functions to use"""
 
 import tweepy, random
-from apscheduler.schedulers.blocking import BlockingScheduler
 import markovgen
-import time
+from time import sleep
 import boooootttt as bot
-
 
 """creates a new instance of boooootttt"""
 
 bot_ = bot.bot()
 
 """creates a new instance of Markov and uses database.txt as an infile."""
-
 with open('database.txt','r') as infile:
 	markov = markovgen.Markov(infile)
 
-"""calls generate_markov_text and sends that to the bot to tweet out.
-	the BlockingScheduler schedules run_tweet as a job and runs it every 1-3
-	hours"""
-	
-def run_tweet():
-	try:
-		text = markov.generate_markov_text()
-		bot_.do_tweet(text)
-	except tweepy.TweepError as e:
-		print(e)
 
-rando = random.randint(3600,10800)
-scheduler = BlockingScheduler()
-scheduler.add_job(run_tweet,'interval', minutes = rando)
-scheduler.start()
+"""calls generate_markov_text and sends that to the bot to tweet out.
+	then the loops sleeps for a random amout of time between one and three
+	hours"""
+
+while True:
+	bot_.check_followers()
+    text = markov.generate_markov_text()
+    #bot_.do_tweet(text)
+    rando = random.randint(3600,10800)
+    sleep(rando)
