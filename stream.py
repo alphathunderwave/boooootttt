b4d1f486728174315186406a41ab3a5a3dab0a40
alphathunderwave <bdlipp@buffalo.edu>
2017-08-13 22:23:37 -0400

    added try catch to stream

--- stream.py
+++ stream.py
@@ -19,27 +19,30 @@ count = 0
 
 class StdOutListener(StreamListener):
 	def on_status(self, status):
+		try:
+			"""this counter only lets the bot run 15 times, then it waits for
+				15 minutes to protect overusing twitter"""
 
-		"""this counter only lets the bot run 15 times, then it waits for
-			15 minutes to protect overusing twitter"""
+			global count
+			count = count + 1
+			if count == 15:
+				sleep(900)
+				count =0
 
-		global count
-		count = count + 1
-		if count == 15:
-			sleep(900)
-			count =0
+			"""the bot wont respond to itself"""
 
-		"""the bot wont respond to itself"""
+			if status.user.screen_name == 'boooootttt_':
+				pass
 
-		if status.user.screen_name == 'boooootttt_':
-			pass
-
-		else:
-			"""this is the general reply for anyone who tagged the bot"""
-			response = markov.generate_markov_text()
-			bot_.reply(status,str(response))
-
+			else:
+				"""this is the general reply for anyone who tagged the bot"""
+				response = markov.generate_markov_text()
+				bot_.reply(status,str(response))
+		except tweepy.TweepError as e:
+			bot_.log(e)
+			
 		return True
+
 
 	"""on_error prints the error status and stops the stream. this should not
 		happen. if it does something is broken and needs to be fixed"""
